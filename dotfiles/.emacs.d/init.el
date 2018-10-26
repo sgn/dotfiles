35cfb5e98bdb4f33a2d5a431aa0b007a4ebc0390
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-10-26 16:18:20 +0700

    emacs: add vimrc mode

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -141,6 +141,21 @@
 (when (require 'flycheck nil t)
   (require 'danh-init-spelling))
 
+;; https://stackoverflow.com/questions/4236808/syntax-highlight-a-vimrc-file-in-emacs/4238738
+(define-generic-mode 'vimrc-generic-mode
+  '()
+  '()
+  '(("^[\t ]*:?\\(!\\|ab\\|map\\|unmap\\)[^\r\n\"]*\"[^\r\n\"]*\\(\"[^\r\n\"]*\"[^\r\n\"]*\\)*$"
+     (0 font-lock-warning-face))
+    ("\\(^\\|[\t ]\\)\\(\".*\\)$"
+     (2 font-lock-comment-face))
+    ("\"\\([^\n\r\"\\]\\|\\.\\)*\""
+     (0 font-lock-string-face)))
+  '("/vimrc\\'" "\\.vim\\(rc\\)?\\'")
+  '((lambda ()
+      (modify-syntax-entry ?\" ".")))
+  "Generic mode for Vim configuration files.")
+
 ;;;; Theme
 (if (custom-theme-p 'dracula)
     (load-theme 'dracula t)
