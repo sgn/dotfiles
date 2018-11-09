9fbf54c2ea625b44c73345a8a929bac4980f9223
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-11-09 22:07:24 +0700

    install: update all submodules

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 CDPATH=
 export CDPATH
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname -- "$0")
 cd "$BASEDIR"
+
+git submodule update --init
+
 mkdir -p ~/.gnupg
 chmod go-rwx ~/.gnupg
 stow dotfiles -t ~
