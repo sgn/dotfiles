7cdea5ac65d30cbed14f29254305379d4c7c68cd
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-11-11 09:02:56 +0700

    refind: short and dirty script to gen refind.conf
    not gonna install refind in this machine

---
+++ dotfiles/.local/bin/refind-gen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat <<EOF
+timeout 20
+scanfor manual
+EOF
+
+for kernel in $(ls /boot/vmlinuz-*); do
+	kernel="${kernel#/boot}"
+	ver_rev="${kernel#/vmlinuz-}"
+	initrd="/initramfs-${ver_rev}.img"
+	cat <<EOF
+menuentry "Void Linux (${ver_rev})" {
+	icon     /EFI/refind/icons/os_void.png
+	volume   b6ede7cb-4592-4fae-98ab-e761fa8dc081
+	loader   ${kernel}
+	initrd   ${initrd}
+	options  "root=PARTUUID=79de8074-3b32-4408-9a56-70e8ace04e43 rw add_efi_memmap quiet"
+}
+EOF
+done
