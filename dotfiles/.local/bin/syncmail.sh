2b815027b73570b6d7fc57950e215c7faccd7b5e
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-11-24 00:14:06 +0700

    syncmail: stop notifying i3blocks

--- dotfiles/.local/bin/syncmail.sh
+++ dotfiles/.local/bin/syncmail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ssoma sync --cron || exit 0
-mbsync -a >/dev/null && pkill -RTMIN+2 i3blocks || exit 0
+mbsync -a >/dev/null || exit 0
 notmuch new 2>&1 >/dev/null | grep -vF 'non-mail'
