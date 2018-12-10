b816dfadc6a41c78293c1deb1e08483dcd76e7d7
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-12-10 21:30:45 +0700

    emacs: change to base16-theme

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -49,9 +49,11 @@
         pass                            ; password-store
         pass-otp
         helm-pass
+        markdown-mode
         helm-youtube
         which-key
         pdf-tools
+        base16-theme
         transmission))
 
 ;;;; Package Management
@@ -166,8 +168,18 @@
 ;; (if (custom-theme-p 'dracula)
 ;;     (load-theme 'dracula t)
 ;;   (message "Notheme"))
-(setq custom-theme-directory "~/.emacs.d/theme/")
-(load-theme 'dracula t)
+;; (setq custom-theme-directory "~/.emacs.d/theme/")
+;; (load-theme 'dracula t)
+(load-theme 'base16-tomorrow-night t)
+;; Set the cursor color based on the evil state
+(defvar danh/base16-colors base16-tomorrow-night-colors)
+(setq evil-emacs-state-cursor   `(,(plist-get danh/base16-colors :base0D) box)
+      evil-insert-state-cursor  `(,(plist-get danh/base16-colors :base0D) bar)
+      evil-motion-state-cursor  `(,(plist-get danh/base16-colors :base0E) box)
+      evil-normal-state-cursor  `(,(plist-get danh/base16-colors :base0B) box)
+      evil-replace-state-cursor `(,(plist-get danh/base16-colors :base08) bar)
+      evil-visual-state-cursor  `(,(plist-get danh/base16-colors :base09) box))
+
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Finalization
 
