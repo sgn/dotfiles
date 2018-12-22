9b2a2a50173fc8dfdc424ee751c0ba7dfc21391a
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2018-12-22 08:24:37 +0700

    syncmail: remove ssoma from syncmail
    and make a better crontab builder

--- croninstall.sh
+++ croninstall.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-tmp=$(mktemp)
-cat <<EOF >| $tmp
-# minute hour day_of_month month day_of_week command
-*/2	 *    *		   *	 *	     ${HOME}/.local/bin/syncmail.sh
-*/5	 *    *		   *	 *	     ${HOME}/.config/dunst/battery.sh
-EOF
+test -z "${XDG_CONFIG_HOME}" && XDG_CONFIG_HOME="$HOME/.config"
+make_cron () {
+	crontab -l 2>/dev/null |
+		sed '/ssoma/d;
+			/local\/bin\/syncmail.sh/d;
+			/dunst\/battery/d'
+	# minute hour day_of_month month day_of_week command
+	cat <<-EOF
+	*/10	 *    *		   *	 *	     ssoma sync --cron
+	*/2	 *    *		   *	 *	     ${HOME}/.local/bin/syncmail.sh
+	*/5	 *    *		   *	 *	     ${XDG_CONFIG_HOME}/dunst/battery.sh
+	EOF
+}
 
-EDITOR="mv -f ${tmp}" VISUAL="mv -f ${tmp}" crontab -e
+make_cron | tee /dev/stderr | crontab -
--- dotfiles/.local/bin/syncmail.sh
+++ dotfiles/.local/bin/syncmail.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-ssoma sync --cron || exit 1
 mbsync -a >/dev/null || exit 1
 notmuch new 2>&1 | grep -vF 'non-mail' || exit 0
