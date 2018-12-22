8e134f001457fa5d4e004f70661c347ea140d629
Danh Doan <congdanhqx@gmail.com>
2018-12-22 10:26:51 +0700

    pam: make envinronment relative to project dir

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+set -e
 CDPATH=
 export CDPATH
-BASEDIR=$(dirname -- "$0")
+BASEDIR=$(readlink -f "$0")
+BASEDIR="${BASEDIR%/*}"
 cd "$BASEDIR"
 
 git submodule update --init
 
 mkdir -p ~/.gnupg
 chmod go-rwx ~/.gnupg
+
+cat <<EOF >| ~/.pam_environment
+DOTFILES_HOME		DEFAULT="${BASEDIR}"
+XDG_CONFIG_HOME		DEFAULT="${BASEDIR}/config"
+SSH_AUTH_SOCK		DEFAULT="\${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+EOF
+
 stow dotfiles -t ~
