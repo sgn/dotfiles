919a8815255262b77dee63968a6d25f4adf5c26e
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2019-01-02 01:21:52 +0700

    Xresource: build Xresource from install script

--- install.sh
+++ install.sh
@@ -11,10 +11,17 @@ git submodule update --init
 mkdir -p ~/.gnupg
 chmod go-rwx ~/.gnupg
 
+XDG_CONFIG_HOME_="${BASEDIR}/config"
+
 cat <<EOF >| ~/.pam_environment
 DOTFILES_HOME		DEFAULT="${BASEDIR}"
-XDG_CONFIG_HOME		DEFAULT="${BASEDIR}/config"
+XDG_CONFIG_HOME		DEFAULT="${XDG_CONFIG_HOME_}"
 SSH_AUTH_SOCK		DEFAULT="\${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+EOF
+
+cat <<EOF >| ~/.Xresources
+#include "${XDG_CONFIG_HOME_}/Xresources/color.conf"
+#include "${XDG_CONFIG_HOME_}/Xresources/urxvt.conf"
 EOF
 
 stow dotfiles -t ~
