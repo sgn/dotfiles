3d9c7f30c3ed253b2065cb523f2f01b5d9ebb2c8
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2019-01-02 10:54:09 +0700

    syncmail: naively reduce parallel mbsync

--- bin/syncmail.sh
+++ bin/syncmail.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if ps -Cmbsync >/dev/null 2>&1; then
+	echo "Another mbsync is running!" >&2
+	exit 1
+fi
+
 mbsync -a >/dev/null || exit 1
 notmuch new 2>&1 | grep -vF 'non-mail' || exit 0
