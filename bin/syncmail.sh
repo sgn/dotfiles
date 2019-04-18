7bc6c2540d0bbf90815e94e2e5f0430f9def09ba
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-04-18 11:04:46 +0700

    syncmail: use quiet option

--- bin/syncmail.sh
+++ bin/syncmail.sh
@@ -5,5 +5,5 @@ if ps -Cmbsync >/dev/null 2>&1; then
 	exit 1
 fi
 
-mbsync -a >/dev/null || exit 1
-notmuch new 2>&1 >/dev/null | grep -vF 'non-mail' || true
+mbsync -aq || exit 1
+notmuch new --quiet 2>&1 | grep -vF 'non-mail' || true
