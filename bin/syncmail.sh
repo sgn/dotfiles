905dbc0069d25994eed43f310e3cef7a32b9b23b
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-04-18 20:39:11 +0700

    syncmail: check for common error before running mbsync
    mbsync fills internal mailbox a lot

--- bin/syncmail.sh
+++ bin/syncmail.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+ping -q -c 1 imap.gmail.com >/dev/null 2>&1 || exit
+
+pass xxx >/dev/null 2>&1 || exit
+
 if ps -Cmbsync >/dev/null 2>&1; then
 	echo "Another mbsync is running!" >&2
 	exit 1
