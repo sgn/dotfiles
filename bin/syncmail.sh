5efc66eb8dd9b5a77e482d85a819779545967ce7
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-05-12 22:05:50 +0700

    syncmail: lock whole syncmail process with lockfile
    Sometime, notmuch takes too much time to finish indexing.

--- bin/syncmail.sh
+++ bin/syncmail.sh
@@ -6,10 +6,13 @@ PASSWORD_STORE_DIR="${HOME}/.cache/opass"
 export PASSWORD_STORE_DIR
 pass xxx >/dev/null 2>&1 || exit
 
-if ps -Cmbsync >/dev/null 2>&1; then
-	echo "Another mbsync is running!" >&2
-	exit 1
+LOCK_FILE=/tmp/syncmail.$(id -u).$(hostname).lock
+if test -f "$LOCK_FILE"; then
+	echo Sync mail is running 2>&1
+	exit
 fi
 
-mbsync -aq || exit 1
-notmuch new --quiet 2>&1
+touch "$LOCK_FILE"
+mbsync -aq
+notmuch new --quiet
+rm "$LOCK_FILE"
