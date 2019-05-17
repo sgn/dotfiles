3f0ad6fc9bca90b2307cefa1d0b9ba01b590e31a
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-05-17 08:27:58 +0700

    zsh: move all options to 10 namespace

---
+++ dotfiles/.config/zsh/00-prof.zsh
@@ -0,0 +1,3 @@
+if [[ $ZSH_PROFILE_RC -gt 0 ]] ; then
+    zmodload zsh/zprof
+fi
---
+++ dotfiles/.config/zsh/10-setopt.zsh
@@ -0,0 +1,31 @@
+# background job
+setopt long_list_jobs
+setopt notify
+setopt no_hup
+setopt print_exit_value
+# cd
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+# completion
+setopt hash_list_all
+setopt complete_in_word
+setopt glob
+setopt extended_glob
+setopt no_glob_dots
+# history
+setopt append_history
+setopt share_history
+setopt extended_history
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+# zle
+setopt no_beep
+setopt unset
+setopt interactive_comments
+## try to avoid the 'zsh: no matches found...'
+setopt no_no_match
+## add `|' to output redirections in the history
+setopt hist_allow_clobber
+## warning if file exists ('cat /dev/null > ~/.zshrc')
+setopt no_clobber
