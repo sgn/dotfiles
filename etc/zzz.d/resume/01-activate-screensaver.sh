ec906f1ab8fbb66919b4a8026c9c84b8dde5525d
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-10-18 15:36:18 +0700

    zzz resume: guard against non-X environment

--- etc/zzz.d/resume/01-activate-screensaver.sh
+++ etc/zzz.d/resume/01-activate-screensaver.sh
@@ -5,5 +5,5 @@ if ! command -v xset >/dev/null 2>&1; then
 fi
 
 for x in /tmp/.X11-unix/*; do
-	DISPLAY=":${x#/tmp/.X11-unix/X}" xset s activate || true
+	[ -S $x ] &&  DISPLAY=":${x#/tmp/.X11-unix/X}" xset s activate
 done
