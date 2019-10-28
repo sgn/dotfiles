f33f05dbc04c6dc3c69e5e533d26a5ddb9475323
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-10-28 21:44:52 +0700

    zsh: proper setup git completion

--- dotfiles/.config/zsh/91-git-completions.zsh
+++ dotfiles/.config/zsh/91-git-completions.zsh
@@ -1,4 +1,5 @@
-if [ -f ~/workspace/git/contrib/completion/git-completion.zsh ]; then
-	zstyle ':completion:*:*:git:*' script \
-		~/workspace/git/contrib/completion/git-completion.zsh
-fi
+[ -f ~/workspace/git/contrib/completion/git-completion.zsh ] || return
+_git() {
+	source ~/workspace/git/contrib/completion/git-completion.zsh "$@"
+}
+compdef _git git gitk
