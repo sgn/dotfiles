2268160d1ad16a3478821dd85ed6d4aa3f78552e
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-11-01 17:44:58 +0700

    zsh: autoload: ignore compiled filename

--- dotfiles/.config/zsh/40-autoload-functions.zsh
+++ dotfiles/.config/zsh/40-autoload-functions.zsh
@@ -2,7 +2,7 @@ fpath=( ~/.config/zsh/functions ${fpath} )
 fpath=( ~/.config/zsh/completions ${fpath} )
 
 typeset -aU ffiles
-ffiles=(~/.config/zsh/functions/**/[^_]*[^~](N.:t))
+ffiles=(~/.config/zsh/functions/**/[^_]*[^(~|.zwc)](N.:t))
 (( ${#ffiles} > 0 )) && autoload -U "${ffiles[@]}"
 unset -v ffiles
 
