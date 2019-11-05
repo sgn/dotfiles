2b77def2ebda6eafcd3fedb339b7ea0b4af0b0aa
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-11-05 14:09:36 +0700

    zsh: use xbps-uunshare to run glibc system

--- dotfiles/.config/zsh/93-xlibc.zsh
+++ dotfiles/.config/zsh/93-xlibc.zsh
@@ -1,6 +1,21 @@
 if [ -f /lib/ld-musl-* ]; then
-	alias xlibc-install='sudo xbps-install -r /xlibc'
-	alias xlibc-remove='sudo xbps-remove -r /xlibc'
-	alias xlibc-query='xbps-query -r /xlibc'
-	alias xzoom="xlibc sh -c 'LD_LIBRARY_PATH=/opt/zoom /opt/zoom/zoom'"
+alias xlibc-install='sudo xbps-install -r /xlibc'
+alias xlibc-remove='sudo xbps-remove -r /xlibc'
+alias xlibc-query='xbps-query -r /xlibc'
+xzoom () {
+	xtmp=$(mktemp -d /tmp/xlibc.XXXXXXXX)
+	xbps-uunshare \
+		-b $xtmp:/tmp \
+		-b $HOME:$HOME \
+		-- /xlibc \
+		sh -c 'LD_LIBRARY_PATH=/opt/zoom exec /opt/zoom/zoom'
+}
+
+skype () {
+	xtmp=$(mktemp -d /tmp/xlibc.XXXXXXXX)
+	xbps-uunshare \
+		-b $xtmp:/tmp \
+		-- /xlibc \
+		skypeforlinux
+}
 fi
