45086d503c8c7006d0e885c875e0c53fb3d89889
Doan Tran Cong Danh <congdanhqx@gmail.com>
2019-11-23 20:26:50 +0700

    sh: alias for clipboard  on Windows
    Time to start new adventure with WSL

--- dotfiles/.config/zsh/80-alias-misc.zsh
+++ dotfiles/.config/zsh/80-alias-misc.zsh
@@ -1,4 +1,9 @@
-alias xclipboard='xclip -selection clipboard'
 alias zprofile='ZSH_PROFILE_RC=1 zsh'
 alias da='du -sch'
 alias ixpaste='curl -F "f:1=<-" ix.io'
+
+if command -v xclip >/dev/null 2>&1; then
+	alias clip='xclip -selection clipboard'
+elif command -v clip.exe >/dev/null 2>&1; then
+	alias clip='clip.exe'
+fi
