ed93b1f5c25f3dd1ece82ed3c0b71b12692d727e
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-07-29 17:49:18 +0700

    build-flags: set CPPFLAGS instead of CFLAGS
    All CPPFLAGS will be (usually) appended to CFLAGS.
    But, some broken build systems ignore it, appends to them.

--- sh/80-functions.sh
+++ sh/80-functions.sh
@@ -18,7 +18,14 @@ add_home_local_bin() {
 }
 
 build_flags() {
-	CFLAGS+=" -fstack-protector-strong -D_FORTIFY_SOURCE=2 -march=native -mtune=native"
-	LDFLAGS+=" -Wl,--as-needed -Wl,-z,relro -Wl,-z,now"
-	export CFLAGS LDFLAGS
+	local _cppflags="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -march=native -mtune=native -pipe -O2 -g"
+	CPPFLAGS="${CPPFLAGS:+$CPPFLAGS }$_cppflags"
+	LDFLAGS="${LDFLAGS:+$LDFLAGS }-Wl,--as-needed -Wl,-z,relro -Wl,-z,now"
+	export CPPFLAGS LDFLAGS
+}
+
+fix_broken_build_flags() {
+	CFLAGS="${CFLAGS:+$CFLAGS }${CPPFLAGS:+$CPPFLAGS}"
+	CXXFLAGS="${CXXFLAGS:+$CXXFLAGS }${CPPFLAGS:+$CPPFLAGS}"
+	export CFLAGS CXXFLAGS
 }
