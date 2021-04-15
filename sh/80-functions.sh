6d9e098527734ce73b0836a10f06edbf010438ce
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2021-04-16 06:58:17 +0700

    zsh: fix build_flags

--- sh/80-functions.sh
+++ sh/80-functions.sh
@@ -19,13 +19,8 @@ add_home_local_bin() {
 
 build_flags() {
 	local _cppflags="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -march=native -mtune=native -pipe -O2 -g"
-	CPPFLAGS="${CPPFLAGS:+$CPPFLAGS }$_cppflags"
-	LDFLAGS="${LDFLAGS:+$LDFLAGS }-Wl,--as-needed -Wl,-z,relro -Wl,-z,now"
-	export CPPFLAGS LDFLAGS
-}
-
-fix_broken_build_flags() {
-	CFLAGS="${CFLAGS:+$CFLAGS }${CPPFLAGS:+$CPPFLAGS}"
-	CXXFLAGS="${CXXFLAGS:+$CXXFLAGS }${CPPFLAGS:+$CPPFLAGS}"
-	export CFLAGS CXXFLAGS
+	CFLAGS="$_cppflags ${CFLAGS:-}"
+	CXXFLAGS="$_cppflags ${CXXFLAGS:-}"
+	LDFLAGS="-Wl,--as-needed -Wl,-z,relro -Wl,-z,now ${LDFLAGS:-}"
+	export CFLAGS CXXFLAGS LDFLAGS
 }
