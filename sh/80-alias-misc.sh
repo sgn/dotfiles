530fb757e7b69d2a9c7e4e7673b41c6fa0895339
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2021-06-03 06:58:22 +0700

    alias: remove nnn (binary removed)

--- sh/80-alias-misc.sh
+++ sh/80-alias-misc.sh
@@ -1,7 +1,6 @@
 alias zprofile='ZSH_PROFILE_RC=1 zsh'
 alias da='du -sch'
 alias ixpaste='curl -F "f:1=<-" ix.io'
-alias nnn='nnn -e'
 
 if command -v xclip >/dev/null 2>&1; then
 	alias clip='xclip -selection clipboard'
