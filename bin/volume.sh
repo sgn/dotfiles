d9a77525291aa3db9d469a0da0ed8e1faed7fbf3
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2021-07-09 17:07:00 +0700

    volume.sh: don't hardcode volume and status index

--- bin/volume.sh
+++ bin/volume.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-case $1 in
-up) amixer set Master 2%+ ;;
-down) amixer set Master 2%- ;;
-toggle) amixer set Master toggle ;;
-*)
-	echo "Usage: volume.sh up|down|toggle"
+chvol() {
+	case $1 in
+	up) amixer set Master 2%+ ;;
+	down) amixer set Master 2%- ;;
+	toggle) amixer set Master toggle ;;
+	*)
+		echo "Usage: volume.sh up|down|toggle" >&2
+		exit 1
+		;;
+	esac
+}
+
+volume=$(chvol "$@" |
+	sed -nE '/:.*%/s/[[:space:]]*(.*:).*[[]([0-9]*%)[]].*[[](on|off)[]].*/\1 \2 [\3]/p')
+
+if [ -z "$volume" ]; then
 	exit 1
-	;;
-esac
-
-volume=$(amixer get Master | awk '/%/{print $6=="[off]"?"off":$4}')
-
-if command -v dunstify >/dev/null 2>&1; then
+elif command -v dunstify >/dev/null 2>&1; then
 	NOTIFY_SEND="dunstify -r 29011"
 elif command -v notify-send >/dev/null 2>&1; then
 	NOTIFY_SEND="notify-send"
 else
+	printf '%s\n' "$volume"
 	exit
 fi
-exec $NOTIFY_SEND -i audio-speakers "Volume: ${volume}"
+exec $NOTIFY_SEND -i audio-speakers "${volume}"
